d72e66ebbeee88b700afaf9cba984f2ebfdea069
genius-Mike <andreucci.michael@itismeucci.com>
2026-05-29 12:31:01 +0200

    Setup python file

---
+++ python/CCL.py
@@ -0,0 +1,11 @@
+with open("input.txt","r") as input:
+    assegnaNomi = input.readline()
+    with open(assegnaNomi+".java", "w") as output:
+        print("ciao")
+
+class LetturaFile:
+    print("qua ci va il metodo per la traduzione dell'input")
+
+class Attributo:
+    def __init__(self):
+        pass
